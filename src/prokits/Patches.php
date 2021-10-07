90f76ea404878c501627ab898b2799c389a8acf2
Blackjack200 <lx826444126@gmail.com>
2021-10-07 12:26:48 +0800

    PM4

--- src/prokits/Patches.php
+++ src/prokits/Patches.php
@@ -6,60 +6,59 @@ namespace prokits;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
+use pocketmine\console\ConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
 use SplFileObject;
 use Throwable;
+use Webmozart\PathUtil\Path;
 
 class Patches extends PluginBase {
-	/** @var Patches */
-	private static $instance = null;
-	
-	private $loaderPath = '';
-	
+	private static self $instance;
+	private string $loaderPath;
+
 	public function onLoad() : void {
 		self::$instance = $this;
-		$this->loaderPath = $this->getDataFolder() . DIRECTORY_SEPARATOR . 'patches' . DIRECTORY_SEPARATOR;
-		//TODO Improve this
-		$str = $this->loaderPath . 'first';
-		@mkdir($str);
-		if(!file_exists($str . DIRECTORY_SEPARATOR . 'bootstrap.php')) {
-			@file_put_contents($str . DIRECTORY_SEPARATOR . 'bootstrap.php' , '<?php
+		$this->loaderPath = Path::join($this->getDataFolder(), 'patches');
+		@mkdir($this->loaderPath);
+		$demoPatch = Path::join($this->loaderPath, 'demo');
+		if (!is_dir($demoPatch)) {
+			@mkdir($demoPatch);
+			if (!file_exists(Path::join($demoPatch, 'bootstrap.php'))) {
+				@file_put_contents(Path::join($demoPatch, 'bootstrap.php'), '<?php
 			use prokits\Patches;
 			Patches::getInstance()->getLogger()->warning(\'This is demo Patch.\');
 			');
+			}
 		}
-		@mkdir($this->loaderPath);
 	}
-	
-	public function onCommand(CommandSender $sender , Command $command , string $label , array $args) : bool {
-		if($sender instanceof ConsoleCommandSender && $label === 'loadPatch') {
-			if(!isset($args[0])) {
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+		if ($sender instanceof ConsoleCommandSender && $label === 'loadPatch') {
+			if (!isset($args[0])) {
 				return false;
 			}
-			$this->loadPatch($this->loaderPath , $args[0]);
+			$this->loadPatch($this->loaderPath, $args[0]);
 		}
 		return true;
-		
+
 	}
-	
+
 	public static function getInstance() : Patches {
 		return self::$instance;
 	}
-	
-	public function loadPatch(string $basePath , string $patchName) : void {
+
+	public function loadPatch(string $basePath, string $patch) : void {
 		try {
 			try {
-				$file = new SplFileObject($basePath . $patchName . DIRECTORY_SEPARATOR . 'bootstrap.php');
-			} catch(Throwable $logicException) {
-				$this->getLogger()->info(TextFormat::RED . "Invalid Patch(or Bad Format).");
+				$file = new SplFileObject(Path::join($basePath, $patch, 'bootstrap.php'));
+			} catch (Throwable $logicException) {
+				$this->getLogger()->error("Invalid Patch $patch");
 				return;
 			}
 			require $file->getRealPath();
-		} catch(Throwable $throwable) {
-			$this->getLogger()->warning('Error when loading ' . $patchName);
-			$this->getLogger()->emergency($throwable);
+		} catch (Throwable $throwable) {
+			$this->getLogger()->error("Error when applying patch $patch");
+			$this->getLogger()->logException($throwable);
 		}
 	}
 }
