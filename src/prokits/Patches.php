d27d513c5a0b353a5a3fcb07b9b7ac107e021724
Blackjack200 <lx826444126@gmail.com>
2024-12-05 21:33:45 +0800

    rewrite

--- src/prokits/Patches.php
+++ src/prokits/Patches.php
@@ -8,7 +8,8 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\console\ConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
-use SplFileObject;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Utils;
 use Symfony\Component\Filesystem\Path;
 use Throwable;
 
@@ -20,11 +21,41 @@ class Patches extends PluginBase {
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-		if ($sender instanceof ConsoleCommandSender && $label === 'loadPatch') {
-			if (!isset($args[0])) {
-				return false;
+		if ($sender instanceof ConsoleCommandSender) {
+			switch ($label) {
+				case 'loadPatch':
+					if (!isset($args[0])) {
+						return false;
+					}
+
+					$path = realpath(Path::join($this->getDataFolder(), $args[0] . ".php"));
+					if ($path === false) {
+						$sender->sendMessage(TextFormat::RED . "Failed to obtain $args[0]'s absolute path");
+						return true;
+					}
+					if (!is_file($path)) {
+						$sender->sendMessage(TextFormat::RED . "$args[0] is not a file");
+						return true;
+					}
+					try {
+						require $path;
+					} catch (Throwable $e) {
+						$sender->sendMessage(TextFormat::YELLOW . "Error while loading patch $args[0]");
+						$sender->sendMessage(implode(TextFormat::YELLOW . "\n", Utils::printableExceptionInfo($e)));
+					}
+					break;
+				case 'listPatch':
+					$dir = scandir($this->getDataFolder(), SCANDIR_SORT_NONE);
+					if ($dir === false) {
+						$sender->sendMessage(TextFormat::RED . "Failed to scan directory");
+						return false;
+					}
+					$dir = array_diff($dir, ['..', '.']);
+					$count = count($dir);
+					$sender->sendMessage(TextFormat::GREEN . "Available patches($count): " . implode(", ", array_map(Path::getFilenameWithoutExtension(...), $dir)));
 			}
-			$this->loadPatch($args[0]);
+		} else {
+			$sender->sendMessage(TextFormat::RED . "This command is only available via the console");
 		}
 		return true;
 
@@ -32,21 +63,5 @@ class Patches extends PluginBase {
 
 	public static function getInstance() : Patches {
 		return self::$instance;
-	}
-
-	public function loadPatch(string $patch) : void {
-		$logger = $this->getLogger();
-		try {
-			try {
-				$file = new SplFileObject(Path::join($this->getDataFolder(), $patch . ".php"));
-			} catch (Throwable) {
-				$logger->error("invalid patch $patch.");
-				return;
-			}
-			require $file->getRealPath();
-		} catch (Throwable $throwable) {
-			$logger->error("error applying patch $patch.");
-			$logger->logException($throwable);
-		}
 	}
 }
